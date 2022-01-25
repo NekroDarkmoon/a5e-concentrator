623ea051829c8625c9266ac11220386bce37a0c1
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-01-25 07:22:43 -0800

    Changed checkbox to select

--- scripts/generateTemplate.js
+++ scripts/generateTemplate.js
@@ -2,7 +2,7 @@
 //                               Imports and Constants
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 import { moduleName, moduleTag } from './constants.js';
-import { templateType, AbilityTemplate } from './AbilityTemplate.js';
+import { AbilityTemplate } from './AbilityTemplate.js';
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                                  Place Template
@@ -14,52 +14,83 @@ export class TemplatePlacer {
 		// this._activateListeners();
 	}
 
+	/**
+	 *
+	 * @param {*} item
+	 * @param {*} save
+	 * @param {*} actor
+	 * @param {*} _data
+	 * @returns
+	 */
 	_onSaveItemRolled(item, save, actor, _data) {
 		// Check Global Settings
 		if (!game.settings.get(moduleName, 'generateTemplate')) return;
 
 		// Ask per item if not set
 		const flag = item.getFlag(moduleName, 'generateTemplate');
-		console.log(flag);
-		if (!flag) return;
+		if (flag === 'none') return;
+
+		if (flag === 'always') {
+			this._placeTemplate(item);
+			return;
+		}
 
 		// Bail out Dialog
 		Dialog.confirm({
 			title: 'Generate Template',
 			content: 'Do you wish to place a template?',
-			yes: html => {
-				// Check if has an area
-				if (!item.data.data.target?.type === 'area') return;
-				const template = AbilityTemplate.fromItem(item);
-				if (template) template.drawPreview();
-			},
+			yes: html => this._placeTemplate(item),
 			no: html => {
 				return;
 			},
 		});
 	}
 
+	/**
+	 *
+	 * @param {*} item
+	 * @returns
+	 */
+	_placeTemplate(item) {
+		// Check if it has an area
+		if (!item.data.data.target?.type === 'area') return;
+		const template = AbilityTemplate.fromItem(item);
+		if (template) template.drawPreview();
+	}
+
+	/**
+	 *
+	 * @param {*} a
+	 * @param {Document} $html
+	 * @param {*} data
+	 * @returns
+	 */
 	_addConfigToSheet(a, $html, data) {
 		const actorId = a._element[0].id.split('-')[1];
 		const itemId = data.item._id;
-		const enabled = game.actors
+		const choice = game.actors
 			.get(actorId)
-			.items.get(itemId)
-			.getFlag(moduleName, 'generateTemplate');
+			?.items.get(itemId)
+			?.getFlag(moduleName, 'generateTemplate');
 
-		// Create element
-		const $div = `<div 
+		if (!choice) return;
+
+		// Create element for generate template
+		const $div1 = `<div 
 				class="a5e-form__section a5e-form__section--inline template-helpers-gt">
 				<h3 class="a5e-form__input-label a5e-form__input-label--inline">
 					Place Template On Use
 				</h3>
-				<div class="a5e-input-container a5e-input-container--inline-wide">
-					<input 
-						type="checkbox" 
-						id="${actorId}-${itemId}" 
+				<div class="a5e-input-container a5e-input-container--inline">
+					<select 
+						class="a5e-select a5e-select--inline" 
 						name="template-helpers-generate"
-						${enabled ? 'checked' : ''}
-					/>
+						id="${actorId}-${itemId}"
+					>
+						<option value="none" ${choice === 'none' ? 'selected' : ''}>None</option>
+						<option value="always" ${choice === 'always' ? 'selected' : ''}>Always</option>
+						<option value="ask" ${choice === 'ask' ? 'selected' : ''}>Ask</option>
+					</select>
 				</div>
 			</div>
 			`;
@@ -67,32 +98,30 @@ export class TemplatePlacer {
 		// Find location and insert
 		const $areaTemplates = $html.find('.a5e-radio-group--area-templates')[0];
 		const $section = $areaTemplates.parentElement;
-		$($section).after($div);
+		$($section).after($div1);
 
 		TemplatePlacer._activateListeners();
 	}
 
+	/**
+	 *
+	 */
 	static _activateListeners() {
 		const $document = document;
 
+		// GenerateTemplate Checkbox
 		$($document)
-			.find('input[name="template-helpers-generate"]')
+			.find('select[name="template-helpers-generate"]')
 			.on('change', async $event => {
 				const target = $event.currentTarget;
-				const isChecked = target.checked;
+				const value = target.value;
 				const actorId = target.id.split('-')[0];
 				const itemId = target.id.split('-')[1];
 
-				if (isChecked)
-					await game.actors
-						.get(actorId)
-						.items.get(itemId)
-						.setFlag(moduleName, 'generateTemplate', true);
-				else
-					await game.actors
-						.get(actorId)
-						.items.get(itemId)
-						.setFlag(moduleName, 'generateTemplate', false);
+				await game.actors
+					.get(actorId)
+					.items.get(itemId)
+					.setFlag(moduleName, 'generateTemplate', value);
 			});
 	}
 }
